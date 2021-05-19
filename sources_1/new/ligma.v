09cdd2d6d849eb494d463a0901cbee207b200694
Connor Azzarello <connor.azzarello@gmail.com>
2021-05-18 17:20:43 -0700

    added ligma test file

---
+++ sources_1/new/ligma.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 05/18/2021 05:20:16 PM
+// Design Name: 
+// Module Name: ligma
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module ligma(
+
+    );
+endmodule
